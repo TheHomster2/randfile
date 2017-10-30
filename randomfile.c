ebf5afd691c15e16a82a04138def0641b462a382
Charles <cweng@stuy.edu>
2017-10-30 02:22:54 -0400

    print statements

--- randomfile.c
+++ randomfile.c
@@ -18,6 +18,7 @@ int main (){
   read(fd, rand1, sizeof(rand1));
   // done with the file
   close(fd);
+  printf("reading 1 done\n");
 
   printf("array 1:\n");
   int i = 0;
@@ -27,12 +28,14 @@ int main (){
     i ++;
   }
 
+
   // write to the file the array
   fd = open("out.txt", O_CREAT | O_WRONLY, 0644);
   // ??write the bit chain rand into the file out.txt??
   write(fd, rand1, sizeof(rand1));
   // done with the file
   close(fd);
+  printf("writng done\n");
 
   fd = open("out.txt", O_RDONLY);
   int rand2[10];
@@ -40,6 +43,7 @@ int main (){
   read(fd, rand2, sizeof(rand2));
   // done with the file
   close(fd);
+  printf("reading 2 done\n");
 
   printf("\n\n\narray 2:\n");
   i = 0;
